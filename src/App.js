79db0d9af900cf3bacd6025aac36e33ff53346ee
Bartosz Czaplewski <bartosz.czaplewski@asist.pl>
2022-06-29 14:45:22 +0200

    structure rebuild

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from "react"
+import Loading from './components/Loading';
+import axios from 'axios'
+import UsersList from './components/UsersList'
+
+function App() {
+  const[users, setUsers] = useState(null)
+
+  useEffect (()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users").then((result)=>{setUsers(result.data)});
+  }, []);
+
+  return <div className="table">{users ? <UsersList users={users}/> : <Loading /> }</div>;
+}
+
+export default App;
